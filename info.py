767c00e75cf89337f166c6a6ca3fc62a5cb7a5a6
Jxjdjjdfhdhd <oppolm1020@gmail.com>
2024-06-09 22:51:08 +0400

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_HASH = environ.get('API_HASH', '9937930c368c669ca905e9a95aa712f0')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
