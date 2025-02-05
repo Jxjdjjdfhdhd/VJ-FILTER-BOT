10f2a3a64e34ff9b80c51ba3868c00e148bd9da9
Jxjdjjdfhdhd <oppolm1020@gmail.com>
2025-02-05 22:14:10 +0400

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -6,10 +6,13 @@
 
 import re
 from Script import script
-from info import API_ID, API_HASH, CLONE_MODE, LOG_CHANNEL
+from info import API_ID, API_HASH, CLONE_MODE, LOG_CHANNEL, DATABASE_URI 
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
 from database.users_chats_db import db
+
+mongo_client = MongoClient(DATABASE_URI)
+mongo_db = mongo_client["cloneLEO"]
 
 @Client.on_message(filters.command('clone'))
 async def clone_menu(client, message):
